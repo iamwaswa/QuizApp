ddbd7cb6b3fdf6a159b3a07e52c45bf5674900c2
waswaolunga <olungaw@gmail.com>
2018-05-12 21:20:37 -0700

    Changed toast message to display above question

--- app/src/main/java/com/example/waswaolunga/quizapp/controller/QuizActivity.java
+++ app/src/main/java/com/example/waswaolunga/quizapp/controller/QuizActivity.java
@@ -3,6 +3,7 @@ package com.example.waswaolunga.quizapp.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,6 +13,8 @@ import com.example.waswaolunga.quizapp.R;
 public class QuizActivity extends AppCompatActivity {
 
     private final String TAG = "Quiz App";
+    private final int X_OFFSET = 0;
+    private final int Y_OFFSET = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,8 +44,9 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void buttonResponse(int responseID) {
-        Toast.makeText(getApplicationContext(), responseID, Toast.LENGTH_SHORT)
-             .show();
+        Toast toast = Toast.makeText(getApplicationContext(), responseID, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, X_OFFSET, Y_OFFSET);
+        toast.show();
 
         Log.i(TAG, getString(responseID));
     }
